3eccb8aff23c1abc5b09caa710e20aace5713740
iqubic <avi.caspe@gmail.com>
2023-12-02 04:00:36 -0800

    rewrite shell.nix

--- shell.nix
+++ shell.nix
@@ -1,12 +1,17 @@
 { pkgs ? import <nixpkgs> {} }:
 let
   src = pkgs.nix-gitignore.gitignoreSource [] ./.;
+  packageSet = pkgs.haskell.packages.ghc92;
+  myPkg = packageSet.callCabal2nix "aoc23" src {};
 in
-pkgs.haskellPackages.shellFor {
-  packages = _: [ (pkgs.haskellPackages.callCabal2nix "aoc22" src {}) ];
+pkgs.stdenv.mkDerivation {
+  name = "aoc-shell";
+
   buildInputs = [
-    pkgs.haskell-language-server
-    pkgs.cabal-install
-    pkgs.hlint
+    myPkg.env.nativeBuildInputs
+
+    packageSet.haskell-language-server
+    packageSet.cabal-install
+    packageSet.hlint
   ];
 }
