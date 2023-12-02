cee841d01fc86ed473084e2f9a27d6a33c3d1a33
iqubic <avi.caspe@gmail.com>
2023-12-02 02:18:47 -0800

    Day 2 refactor

--- src/Day02.hs
+++ src/Day02.hs
@@ -2,40 +2,40 @@ module Day02 where
 
 import Common.Runner
 import Common.Parser
-import Common.Util
-import Control.Applicative (asum)
 import Data.Semigroup
 import Data.Maybe (mapMaybe)
+import Data.Bifunctor (second)
+import Data.Map (Map)
+import Data.Map qualified as M
 
 part1 :: String -> Int
 part1 = sum
-      . mapMaybe (\(Game n xs) -> if validGame xs then Just n else Nothing )
+      . mapMaybe (\(Game n xs) -> if validGame xs then Just n else Nothing)
       . pInput
   where
     validGame = all validPull
     validPull = all validColor
-    validColor (R n) = n <= 12
-    validColor (G n) = n <= 13
-    validColor (B n) = n <= 14
+    validColor (R, n) = n <= 12
+    validColor (G, n) = n <= 13
+    validColor (B, n) = n <= 14
 
 part2 :: String -> Int
 part2 = sum
       . map (\(Game _ xs) -> power xs)
       . pInput
   where
-    -- faldMap gets the max for each color across each pull
-    power xs = let (Max r,Max g,Max b) = foldMap pullSummary xs in
-      r*g*b
-    -- Aggregate the pull colors. Because I don't sort things,
-    -- I need to do this jank
-    pullSummary = foldr toMonoid mempty
-    toMonoid (R n) (_,g,b) = (Max n, g,     b    )
-    toMonoid (G n) (r,_,b) = (r,     Max n, b    )
-    toMonoid (B n) (r,g,_) = (r,     g,     Max n)
+    -- power uses a Map to store the max seen for each color
+    power :: [Pull] -> Int
+    power xs = let pulls = M.unionsWith (<>) (map pullSummary xs) in
+      product $ fmap getMax pulls
+    -- Use Max to compute the max
+    pullSummary :: [Cube] -> Map Color (Max Int)
+    pullSummary = M.fromList . map (second Max)
 
-data Game = Game Int [Pull]
-type Pull = [Cube]
-data Cube = R Int | G Int | B Int
+data Game  = Game Int [Pull]
+type Pull  = [Cube]
+type Cube  = (Color, Int)
+data Color = R | G | B deriving (Eq, Ord, Show)
 
 pInput :: String -> [Game]
 pInput = pLines $ do
@@ -45,11 +45,11 @@ pInput = pLines $ do
       pPull = pCube `sepBy1` string ", "
       pCube = do
         n <- pNumber <* space
-        f <- asum [ string "red"   $> R
-                  , string "blue"  $> B
-                  , string "green" $> G
-                  ]
-        pure $ f n
+        c <- choice [ string "red"   $> R
+                    , string "blue"  $> B
+                    , string "green" $> G
+                    ]
+        pure (c, n)
 
 solve :: Show a => (String -> a) -> IO (Either AoCError a)
 solve = runSolutionOnInput 2
